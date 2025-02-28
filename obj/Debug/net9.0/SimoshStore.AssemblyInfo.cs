0aa01fd042964079abbe36e7d3d90011bae20b87
Mustafa Can Sargın <165585760+mustafas4rgin@users.noreply.github.com>
2025-02-28 13:15:40 +0300

    hata düzeltme

--- obj/Debug/net9.0/SimoshStore.AssemblyInfo.cs
+++ obj/Debug/net9.0/SimoshStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimoshStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b3257c944e6904f443243456c75d205e7db6ac7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca92752f9b3fc9a735635837244d1c6ec6360475")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimoshStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimoshStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- obj/Release/net9.0/SimoshStore.AssemblyInfo.cs
+++ obj/Release/net9.0/SimoshStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimoshStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+305acef8cde60d09afa5f4125e8c42a3170cb709")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca92752f9b3fc9a735635837244d1c6ec6360475")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimoshStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimoshStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
